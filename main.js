0805c782d4a430a5db68ce4330841c32fb8fd0f6
Fawaz Mohamed <fwz.mhmd@gmail.com>
2017-05-10 14:56:14 +0100

    Added if statement for entering no values

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // > greater than
 // < less than
 
-var getWeight, getHeight, weight, height, measure, bmi ;
+var getWeight, getHeight, weight, height, measure, bmi, error ;
 
+// document.getElementById("results").addEventListener('click',calculate,true);
 
 function calculate() {
-   weight = document.getElementById("weight").value;
-   height = document.getElementById("height").value;
-   height = height/100;
-   console.log(height);
-   height = height * height;
-   console.log(height);
-   bmi = weight/height;
+	weight = document.getElementById("weight").value;
+	height = document.getElementById("height").value;
+	error = "Please enter some values";
+	height = height/100;
+	height = height * height;
+	bmi = weight/height;
+
 	if (bmi <= 18.4) {
 		measure = "You are Underweight";
 	} else if (bmi >= 18.5 && bmi <= 24.9) {
@@ -21,5 +22,15 @@ function calculate() {
 	} else if (bmi >= 30) {
 		measure = "You are Obese";
 	}
-	document.getElementById("results").innerHTML = measure;
+	
+
+	if (weight === 0 ) {
+		document.getElementById("results").innerHTML = error;
+	} else if (height === 0){
+		document.getElementById("results").innerHTML = error;
+	}
+	 else {
+
+		document.getElementById("results").innerHTML = measure;
+	}
 }
