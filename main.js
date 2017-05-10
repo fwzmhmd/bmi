239562d6f54580540dcddf88e5ae2175f0f3fbe8
Fawaz Mohamed <fwz.mhmd@gmail.com>
2017-05-10 13:54:35 +0100

    Changed prompt function to getElementById

--- main.js
+++ main.js
@@ -5,21 +5,19 @@ var getWeight, getHeight, weight, height, measure, bmi ;
 
 
 function calculate() {
-   getWeight = prompt("Weight","enter in your weight");
-   getHeight = prompt("ewrwf","enter in your height");
-   weight = parseInt(getWeight);
-   height = parseInt(getHeight);
+   weight = document.getElementById("weight");
+   height = document.getElementById("height");
    height = height/100;
    height = height * height;
    bmi = weight/height;
 	if (bmi >= 0 && bmi <= 18.4) {
-		measure = "Underweight";
+		measure = "You are Underweight";
 	} else if (bmi >= 18.5 && bmi <= 24.9) {
-		measure = "Normal";
+		measure = "You are Normal";
 	} else if (bmi >= 25 && bmi <= 29.9) {
-		measure = "Overweight";
+		measure = "You are Overweight";
 	} else if (bmi >= 30) {
-		measure = "Obese";
+		measure = "You are Obese";
 	}
 	document.getElementById("results").innerHTML = measure;
 }
