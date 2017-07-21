829ab301c39be8b6658109c35f2472a7b7623962
Fawaz Mohamed <fwz.mhmd@gmail.com>
2017-07-21 09:30:10 +0100

    Added a calculate function to calculate user bmi

--- main.js
+++ main.js
@@ -57,3 +57,47 @@ function showImperial() {
 
 }
 
+function calculate() {
+	var feet, inches, pounds, cm, kg, age, bmi, imperialHeight;
+	
+	feet = document.getElementById("feet").value;
+	inches = document.getElementById("inches").value;
+	pounds = document.getElementById("pounds").value;
+	kg = document.getElementById("kg").value;
+	age = document.getElementById("age").value;
+	cm = document.getElementById("cm").value;
+
+	// Caluclate Imperial Height
+	feet = feet * 30.48;
+	inches = inches * 2.54;
+
+	imperialHeight = feet + inches;
+
+	imperialHeight = imperialHeight / 100;
+
+	imperialHeight = imperialHeight * imperialHeight;
+
+
+	// Calculate Imperial Weight
+
+	pounds = pounds * 0.453592;
+	pounds = Math.round(pounds);
+
+	console.log(imperialHeight);
+
+	// Calculate cm into metres
+	cm = cm / 100;
+	cm = cm * cm;
+
+	// Calculate BMI
+	bmi = kg / cm;
+	bmi = pounds / imperialHeight;
+	console.log(bmi);
+
+	// Calculate feet
+
+	// bmi = Math.round(bmi);
+	bmi = Math.round(bmi*10)/10;
+	console.log(bmi);
+	
+}
